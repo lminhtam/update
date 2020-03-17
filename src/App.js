a25098af4ce69747ef50a3645a1e413ab147fbb7
Luong Minh Tam <tam.luong-minh@internal.banvien.com>
2020-03-17 17:44:08 +0700

    config firebase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Card } from "antd";
+const { Meta } = Card;
+
+const data = [
+  {
+    title: "AAAA",
+    cover: "https://nguoinoitieng.tv/images/nnt/96/0/bby1.jpg",
+    description: "BBBBBBBB fnkjskfjkjf ksnfknvkjr jnkncue"
+  },
+  {
+    title: "AAAA",
+    cover: "https://nguoinoitieng.tv/images/nnt/96/0/bby1.jpg",
+    description: "BBBBBBBB fnkjskfjkjf ksnfknvkjr jnkncue"
+  },
+  {
+    title: "AAAA",
+    cover: "https://nguoinoitieng.tv/images/nnt/96/0/bby1.jpg",
+    description: "BBBBBBBB fnkjskfjkjf ksnfknvkjr jnkncue"
+  }
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="leftHalf">
+        {data.map(item => (
+          <Card
+            style={{ width: 300 }}
+            cover={
+              <img alt="example" src={item.cover} width="300" height="150" />
+            }
+          >
+            <Meta title={item.title} description={item.description} />
+          </Card>
+        ))}
+      </div>
+      <div className="rightHalf"></div>
     </div>
   );
 }
---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCIbYZAKvEmY9ns42xafyV54BFU6qUCmKI",
+  authDomain: "dau-giau.firebaseapp.com",
+  databaseURL: "https://dau-giau.firebaseio.com",
+  projectId: "dau-giau",
+  storageBucket: "dau-giau.appspot.com",
+  messagingSenderId: "801715659405",
+  appId: "1:801715659405:web:cb3eecc4840cd11abaf8c4",
+  measurementId: "G-XCRXMT5JYZ"
+};
