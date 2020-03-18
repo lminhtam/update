18194e6e7f0a4df5d1bbeb758c4a065acd3d7af6
Luong Minh Tam <tam.luong-minh@internal.banvien.com>
2020-03-18 15:52:10 +0700

    Updates

---
+++ precache-manifest.efaf6a005d26fc3723f9f3bfc6bab60d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62296904366d69aae3fd96fc2f26c81b",
+    "url": "/update/index.html"
+  },
+  {
+    "revision": "e5d7b572bb8cd81bdb65",
+    "url": "/update/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "e05c2a6a2da84ad3bc5c",
+    "url": "/update/static/css/main.b0aa5460.chunk.css"
+  },
+  {
+    "revision": "e5d7b572bb8cd81bdb65",
+    "url": "/update/static/js/2.aac427a8.chunk.js"
+  },
+  {
+    "revision": "3272418a5f03796eea40d8ddb7225b90",
+    "url": "/update/static/js/2.aac427a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e05c2a6a2da84ad3bc5c",
+    "url": "/update/static/js/main.5bbb3197.chunk.js"
+  },
+  {
+    "revision": "8c8e6d7c356ab8759fb8",
+    "url": "/update/static/js/runtime-main.da2fd39a.js"
+  }
+]);
